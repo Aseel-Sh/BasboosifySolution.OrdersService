fd8efb05e0f70438261b231b04a147dc20e2bab3
Aseel-Sh <aseel.sh52@gmail.com>
2025-09-01 18:28:55 -0500

    Added mapping

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Basboosify.OrdersMicroservice.BusinessLogicLayer.Validators;
+﻿using Basboosify.OrdersMicroservice.BusinessLogicLayer.Mappers;
+using Basboosify.OrdersMicroservice.BusinessLogicLayer.Validators;
 using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,8 @@ public static class DependencyInjection
     {
         //TODO: Add data access layer services into the IoC container
         services.AddValidatorsFromAssemblyContaining<OrderAddRequestValidator>();
+
+        services.AddAutoMapper(typeof(OrderAddRequestToOrderMappingProfile).Assembly);
 
         return services;
     }
---
+++ BusinessLogicLayer/Mappers/OrderItemUpdateRequestToOrderItemMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Basboosify.OrdersMicroservice.BusinessLogicLayer.DTO;
+using Basboosify.OrdersMicroservice.DataAccessLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basboosify.OrdersMicroservice.BusinessLogicLayerr.Mappers;
+
+public class OrderItemUpdateRequestToOrderItemMappingProfile : Profile
+{
+    public OrderItemUpdateRequestToOrderItemMappingProfile()
+    {
+        CreateMap<OrderItemUpdateRequest, OrderItem>()
+          .ForMember(dest => dest.ProductID, opt => opt.MapFrom(src => src.ProductID))
+          .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.UnitPrice))
+          .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
+          .ForMember(dest => dest.TotalPrice, opt => opt.Ignore())
+          .ForMember(dest => dest._id, opt => opt.Ignore());
+    }
+}
